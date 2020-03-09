da99c1990d059d0372b956b53fbfa40cd1268625
Alison Law <alisparklaw@gmail.com>
2020-03-09 16:26:33 +0000

    Tidy

--- index.js
+++ index.js
@@ -9,31 +9,17 @@ const app = express();
 app.use(express.json());
 // STEP 5 decide a port number
 const PORT = 3333;
-// STEP 6 make an endpoint on "/hello" [GET] that sends back a json { "message": "hello" }
-app.get(
-  "/hello",
-  (
-    req, // req is the "objectified" version of the actual http request (piece of paper with 3 parts...)
-    res // res is the toolbox that allows to shape a response and send it back to the client
-  ) => {
-    // set a success code of 200
-    // send back a json response
-    console.log(req.headers);
-    res.status(200).json({ message: "hello" });
-  }
-);
-// STEP 6.5: build an endpoint [GET] "/users" and returns ALL users in the database
-// we will take advantage of the "find" helper from the db.js file inside of data
+// STEP 7 make  endpoints
+
+//GET all
 app.get("/users", async (req, res) => {
   const users = await helpers.find();
   console.log(users);
   res.status(200).json({ users });
 });
-// STEP 6.6 build and endpoint [GET] "/users/:id"
-// sends back the user with the given id
-// we can find the id inside the req object
-// the id can be found inside req.params.id
+// GET by Id
 app.get("/users/:id", (req, res) => {
+  // the id can be found inside req.params.id
   const { id } = req.params;
   helpers
     .findById(id)
@@ -54,10 +40,8 @@ app.get("/users/:id", (req, res) => {
     .catch(error => {
       console.log(error);
     });
-  // the id can be found inside req.params.id
 });
-// STEP 6.7 build a [POST] on "/users"
-// to create a new user using the body of the request
+// POST new thing (insert)
 app.post("/users", async (req, res) => {
   const payload = req.body;
   helpers
@@ -68,7 +52,8 @@ app.post("/users", async (req, res) => {
           .staus(400)
           .json({ errorMessage: "Please provide name and bio for the user." });
       } else {
-        res.status(200).json({ message: "added to DB" });
+        console.log("lady gaga" + {});
+        res.status(200).json(payload);
       }
     })
     .catch(error => {
@@ -78,7 +63,7 @@ app.post("/users", async (req, res) => {
       });
     });
 });
-
+//PUT edit thing (update)
 app.put("/users/:id", async (req, res) => {
   const { id } = req.params;
   const payload = req.body;
@@ -90,12 +75,12 @@ app.put("/users/:id", async (req, res) => {
     } else if (!id) {
       res.status(400).json({ message: "updated" });
     } else {
-      res
-        .status(200)
-        .json({ message: "success"+ id });
+      res.status(200).json({ message: "success" });
     }
   });
 });
+
+//Delete thing (update)
 app.delete("/users/:id", async (req, res) => {
   const { id } = req.params;
   helpers.remove(id).then(id => {
@@ -108,7 +93,7 @@ app.delete("/users/:id", async (req, res) => {
     }
   });
 });
-// STEP 7 make the express app listen on PORT
+// STEP 6 make the express app listen on PORT
 app.listen(PORT, () => {
   console.log(`Great! Listening on ${PORT}`);
 });
